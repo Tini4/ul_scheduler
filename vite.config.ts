1f510c003de09435508264f34127d873a6d558a4
Žiga Slatnar Štagar <99482159+Privatech38@users.noreply.github.com>
2025-09-14 13:37:19 +0200

    Updated vite to 7.1.5 and necessary dependencies (#2)
    * Updated vite to 7.1.5 and necessary dependencies
    
    * Add terser dependency

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [vue()],
     build: {
+        minify: 'terser',
         terserOptions: {
             mangle: false,
         },
